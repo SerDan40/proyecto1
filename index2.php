999037b14ef1d5895680dcf9b9f5c916d040cacd
Misael <misaelpayret@gmail.com>
2025-05-27 23:40:47 -0300

    Cambio de Index2.php

--- index2.php
+++ index2.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Segundo INDEX</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            background-color: black;
+        }
+        h1 {
+            color: white;
+        }
+    </style>
 </head>
 <body>
-    
+    <h1> Bienvenido a mi Index2 BY: Misael</h1>
 </body>
 </html>
